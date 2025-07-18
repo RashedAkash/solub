a0d70eb60373bc684304d9248ad85afd55abb9da
RashedAkash <rashedhossain416400@gmail.com>
2025-07-18 18:36:27 +0600

    text-slider create

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,46 +1,57 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-  
-    // data bg img 
-    $("[data-bg-img]").each(function () {
-        $(this).css("background-image", "url(" + $(this).attr("data-bg-img") + ")")
-    })
+  // data bg img
+  $("[data-bg-img]").each(function () {
+    $(this).css("background-image", "url(" + $(this).attr("data-bg-img") + ")");
+  });
 
-    // data border color
-    $("[data-border-color]").each(function () {
-        $(this).css("border-color", $(this).attr("data-border-color"))
-    })
+  // data border color
+  $("[data-border-color]").each(function () {
+    $(this).css("border-color", $(this).attr("data-border-color"));
+  });
 
-    // data bg color
-    $("[data-bg-color]").each(function () {
-        $(this).css("background-color", $(this).attr("data-bg-color"))
-    })
+  // data bg color
+  $("[data-bg-color]").each(function () {
+    $(this).css("background-color", $(this).attr("data-bg-color"));
+  });
 
-    // data bg color
-    $("[data-color]").each(function () {
-        $(this).css("color", $(this).attr("data-color"))
-    })
+  // data bg color
+  $("[data-color]").each(function () {
+    $(this).css("color", $(this).attr("data-color"));
+  });
 
+  $(".popup-image").magnificPopup({
+    type: "image",
+    // other options
+  });
 
-    $('.popup-image').magnificPopup({
-        type: 'image'
-        // other options
-    });
+  $(".popup-video").magnificPopup({
+    type: "iframe",
+    // other options
+  });
+  //causes slider
+  var swiper = new Swiper(".akcauses-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+      nextEl: ".akcauses-next",
+      prevEl: ".akcauses-prev",
+    },
+  });
 
-    $('.popup-video').magnificPopup({
-        type: 'iframe'
-        // other options
-    });
-//causes slider
-     var swiper = new Swiper(".akcauses-slider", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-      navigation: {
-        nextEl: ".akcauses-next",
-        prevEl: ".akcauses-prev",
-      },
-    });
-
+  //text slider
+  var swiper = new Swiper(".textslider", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    speed: 4000, // smooth slow speed
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    allowTouchMove: false,
+    grabCursor: false,
+  });
 })(jQuery);
