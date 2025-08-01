08b683dcc4c49585ff2eb165e0af1db2e485b98f
RashedAkash <rashedhossain416400@gmail.com>
2025-08-01 19:58:39 +0600

    services2 area added

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,6 +1,19 @@
 (function ($) {
   "use strict";
 
+
+  //offcanvas
+  $(".akheader-bars").on('click',function(){
+  $(".akoffcanvas-box").addClass("akoffcanvas-active");
+  $(".offcanvas-overlay").addClass("offcanvas-overlay-active");
+  
+});
+
+  $(".akoffcanvas-button-close,offcanvas-overlay").on('click',function(){
+  $(".akoffcanvas-box").removeClass("akoffcanvas-active");
+  
+});
+
   // data bg img
   $("[data-bg-img]").each(function () {
     $(this).css("background-image", "url(" + $(this).attr("data-bg-img") + ")");
